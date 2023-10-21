a72bbf2108208cb2b2db6e96cf12c7bf6f78e77c
Ahmet Bogar <ahmetbogar@hotmail.com>
2023-10-21 21:25:12 +0300

    Add project files.

---
+++ FormsApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using FormsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace FormsApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index(string searchString)
+		{
+			var products = Repository.Products;
+
+			if (!string.IsNullOrEmpty(searchString))
+			{
+				ViewBag.SearchString=searchString;
+				products=products.Where(p=>p.Name.ToLower().Contains(searchString)).ToList();
+			}
+			return View(products);
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+	}
+}
---
+++ FormsApp/Models/Repository.cs
@@ -0,0 +1,39 @@
+﻿namespace FormsApp.Models
+{
+	public class Repository
+	{
+		private static readonly List<Product> _products = new();
+		private static readonly List<Category> _categories = new();
+		public static List<Product> Products
+		{
+			get
+			{
+				return _products;
+			}
+		}
+
+		static Repository()
+		{
+			_categories.Add(new Category { CategoryId=1,Name="Bilgisayar"});
+			_categories.Add(new Category { CategoryId=2,Name="Telefon"});
+
+			_products.Add(new Product { ProductId=1,Name="Iphone 14",Price=40000,IsActive=true,Image="1.jpg",CategoryId=1});
+			_products.Add(new Product { ProductId=2,Name="Iphone 15",Price=50000,IsActive=true,Image="2.jpg",CategoryId=1});
+			_products.Add(new Product { ProductId=3,Name="Iphone 16",Price=60000,IsActive=true,Image="3.jpg",CategoryId=1});
+			_products.Add(new Product { ProductId=4,Name="Iphone 17",Price=70000,IsActive=true,Image="4.jpg",CategoryId=1});
+
+			_products.Add(new Product { ProductId=5,Name="Macbook 14",Price=80000,IsActive=true,Image="5.jpg",CategoryId=2});
+			_products.Add(new Product { ProductId=6,Name= "Macbook 15", Price=95000,IsActive=true,Image="6.jpg",CategoryId=2});
+
+		}
+
+		public static List<Category> Categories
+		{
+			get
+			{
+				return _categories;
+			}
+		}
+
+	}
+}
