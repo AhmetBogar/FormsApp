32d8bff8ccb62092034c325290ab783c380e211e
Ahmet Bogar <ahmetbogar@hotmail.com>
2023-10-24 03:51:34 +0300

    24.10.23

--- FormsApp/Controllers/HomeController.cs
+++ FormsApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using FormsApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 
@@ -7,21 +8,87 @@ namespace FormsApp.Controllers
 {
 	public class HomeController : Controller
 	{
-		public IActionResult Index(string searchString)
+		public IActionResult Index(string searchString, string category)
 		{
 			var products = Repository.Products;
 
 			if (!string.IsNullOrEmpty(searchString))
 			{
-				ViewBag.SearchString=searchString;
-				products=products.Where(p=>p.Name.ToLower().Contains(searchString)).ToList();
+				ViewBag.SearchString = searchString;
+				products = products.Where(p => p.Name!.ToLower().Contains(searchString)).ToList();
 			}
-			return View(products);
+
+			if (!string.IsNullOrEmpty(category))
+			{
+				products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();
+			}
+
+			//ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name",category);
+
+			var model = new ProductViewModel
+			{
+				Products = products,
+				Categories = Repository.Categories,
+				SelectedCategory = category
+			};
+			return View(model);
 		}
 
-		public IActionResult Privacy()
+
+		[HttpGet]
+		public IActionResult Create()
 		{
+			ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
 			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create(Product model, IFormFile imageFile)
+		{
+			var allowedExtensions = new[] { ".jpg", "jpeg", ".png" };
+			var extension = Path.GetExtension(imageFile.FileName);
+			var randomFileName = string.Format($"{Guid.NewGuid().ToString()}{extension}");
+			var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", randomFileName);
+
+			if (imageFile != null)
+			{
+				if (!allowedExtensions.Contains(extension))
+				{
+					ModelState.AddModelError("", "Geçerli bir resim seçiniz.");
+				}
+			}
+
+			if (ModelState.IsValid)
+			{
+				if (imageFile != null)
+				{
+					using (var stream = new FileStream(path, FileMode.Create))
+					{
+						await imageFile.CopyToAsync(stream);
+					}
+				}
+
+				model.Image = randomFileName;
+				model.ProductId = Repository.Products.Count + 1;
+				Repository.CreateProduct(model);
+				return RedirectToAction("Index");
+			}
+			ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+			return View(model);
+		}
+		public IActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+			var entity = Repository.Products.FirstOrDefault(p => p.ProductId == id);
+			if (entity == null)
+			{
+				return NotFound();
+			}
+			ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+			return View(entity);
 		}
 	}
 }
