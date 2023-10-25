62ba2fe902a98ac175214c5f692cb5640979181a
Ahmet Bogar <ahmetbogar@hotmail.com>
2023-10-25 16:59:06 +0300

    25.10.23

--- FormsApp/Models/Repository.cs
+++ FormsApp/Models/Repository.cs
@@ -17,9 +17,9 @@
 			_categories.Add(new Category { CategoryId=1,Name="Bilgisayar"});
 			_categories.Add(new Category { CategoryId=2,Name="Telefon"});
 
-			_products.Add(new Product { ProductId=1,Name="Iphone 14",Price=40000,IsActive=true,Image="1.jpg",CategoryId=2});
+			_products.Add(new Product { ProductId=1,Name="Iphone 14",Price=40000,IsActive=false,Image="1.jpg",CategoryId=2});
 			_products.Add(new Product { ProductId=2,Name="Iphone 15",Price=50000,IsActive=true,Image="2.jpg",CategoryId=2});
-			_products.Add(new Product { ProductId=3,Name="Iphone 16",Price=60000,IsActive=true,Image="3.jpg",CategoryId=2});
+			_products.Add(new Product { ProductId=3,Name="Iphone 16",Price=60000,IsActive=false,Image="3.jpg",CategoryId=2});
 			_products.Add(new Product { ProductId=4,Name="Iphone 17",Price=70000,IsActive=true,Image="4.jpg",CategoryId=2});
 
 			_products.Add(new Product { ProductId=5,Name="Macbook 14",Price=80000,IsActive=true,Image="5.jpg",CategoryId=1});
@@ -32,6 +32,43 @@
 			_products.Add(entity);
 		}
 
+		public static void EditProduct(Product updatedProduct)
+		{
+			var entity=_products.FirstOrDefault(p=>p.ProductId==updatedProduct.ProductId);
+
+			if(entity != null)
+			{
+				if (!string.IsNullOrEmpty(updatedProduct.Name))
+				{
+					entity.Name=updatedProduct.Name;
+				}
+				entity.Price = updatedProduct.Price;
+				entity.Image = updatedProduct.Image;
+				entity.CategoryId = updatedProduct.CategoryId;
+				entity.IsActive = updatedProduct.IsActive;
+			}
+		}
+
+		public static void EditIsActive(Product updatedProduct)
+		{
+			var entity = _products.FirstOrDefault(p => p.ProductId == updatedProduct.ProductId);
+
+			if (entity != null)
+			{
+				entity.IsActive = updatedProduct.IsActive;
+			}
+		}
+
+		public static void DeleteProduct(Product deletedProduct)
+		{
+			var entity=_products.FirstOrDefault(p=>p.ProductId== deletedProduct.ProductId);
+
+			if(entity != null)
+			{
+				_products.Remove(entity);
+			}
+		}
+
 		public static List<Category> Categories
 		{
 			get
@@ -39,6 +76,5 @@
 				return _categories;
 			}
 		}
-
 	}
 }
